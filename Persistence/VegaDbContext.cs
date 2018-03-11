4d903f33408a86ef793c3da5308d0bfb14985c93
maccarov <maccarov@gmail.com>
2018-03-11 14:21:11 +0300

    Раздел 2. Лекция 28

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using vega.Models;
 
 namespace vega.Persistence
 {
@@ -7,7 +8,7 @@ namespace vega.Persistence
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
             : base(options)
         {
-            
         }
+        public DbSet<Make> Makes { get; set; }
     }
 }
